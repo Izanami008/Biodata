c3af61dd164c931cd620364ee21c3bfb4aea839f
Izanami008 <162397959+Izanami008@users.noreply.github.com>
2026-04-13 13:37:05 +0700

    Export speak function and adjust pitch

--- pages/ai/voice.js
+++ pages/ai/voice.js
@@ -1,7 +1,9 @@
-function speak(text){
+export function speak(text){
+
 let msg=new SpeechSynthesisUtterance(text);
 msg.lang="id-ID";
-msg.pitch=1.4;
+msg.pitch=1.5;
 msg.rate=0.9;
+
 speechSynthesis.speak(msg);
 }
